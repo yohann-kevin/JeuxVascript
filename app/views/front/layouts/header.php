06d91b114790424875c9f2d2951c34edf420618c
Perriguey <yo44prg@icloud.com>
2020-03-24 16:32:19 +0100

    modify head

--- app/views/front/layouts/header.php
+++ app/views/front/layouts/header.php
@@ -1,37 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- token google for google connect -->
-    <meta name="google-signin-client_id" content="991297201856-i8t0ekurllat0825dfs4tdlr6f5qbqn1.apps.googleusercontent.com">
-    <!-- lien vers les feuilles de style -->
-    <link rel="stylesheet" href="app/public/style/header.css">
-    <link rel="stylesheet" href="app/public/style/home.css">
-    <link rel="stylesheet" href="app/public/style/footer.css">
-    <link rel="stylesheet" href="app/public/style/mediaQueries.css">
-    <link rel="stylesheet" href="app/public/style/contact.css">
-    <link rel="stylesheet" href="app/public/style/snake.css">
-    <link rel="stylesheet" href="app/public/style/register.css">
-    <link rel="stylesheet" href="app/public/style/news.css">
-    <link rel="stylesheet" href="app/public/style/about.css">
-    <link rel="stylesheet" href="app/public/style/article.css">
-    <link rel="stylesheet" href="app/public/style/game.css">
-    <link rel="stylesheet" href="app/public/style/space.css">
-    <link rel="stylesheet" href="app/public/style/account.css">
-    <link rel="stylesheet" href="app/public/style/usersArticle.css">
-    <link rel="stylesheet" href="app/public/style/userSettings.css">
-    <link rel="stylesheet" href="app/public/style/stats.css">
-    <!-- icône -->
-    <link rel="icon" href="app/public/images/logo/icone.png">
-    <!-- charge la biblitoheque chart js -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
-    <!-- Charge la bibliothèque jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <title>test</title>
-</head>
-
 <body>
     <header id="header">
         <!-- <img src="Jeuxvacsript.png" alt="Jeuxvacsript"> -->
--- app/views/front/usersArticle.php
+++ app/views/front/usersArticle.php
@@ -1,3 +1,4 @@
+<?php require_once 'app/views/front/layouts/head.php'; ?>
 <?php include_once 'app/views/front/layouts/header.php'; ?>
 
     <main id="usersArticle">
