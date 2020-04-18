fdc8de29bb56edb44f2a3e58b578e59f59149aa0
Perriguey <yo44prg@icloud.com>
2020-04-18 16:52:59 +0200

    add counter 2

---
+++ app/public/script/index.js
@@ -0,0 +1,23 @@
+
+function Counter(id, index, speed, limit) {
+    var counterId = document.getElementById(id);
+    var timer;
+    this.count = count();
+
+    function count() {
+        if (index === limit) {
+            clearTimeout(timer);
+        } else {
+            counterId.innerHTML = index;
+            index++;
+            setTimeout(count, speed);
+        }
+    }
+
+}
+
+// window.onload = function () {
+//     new Counter('c1', 0, 25, 100);
+//     new Counter('c2', 0, 50, 50);
+//     new Counter('c3', 0, 100, 24);
+// }
