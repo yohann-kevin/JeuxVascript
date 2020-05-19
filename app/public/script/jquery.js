efc685004abc7236455b7fa0c873131342dbf045
Perriguey <yo44prg@icloud.com>
2020-05-19 14:31:54 +0200

    delete about js

--- app/public/script/jquery.js
+++ app/public/script/jquery.js
@@ -1,3 +1,37 @@
+(function($) {
+    $.fn.hideElement = function(event,element,element2) {
+        $(event).click(function() {
+            $(element).hide("slide", {direction: "right"}, 1000, function() {
+                $(event).hide("slide", {direction: "left"}, 1000, function() {
+                    $(element2).show(1000)
+                }); 
+            });
+        });
+    };
+})(jQuery);
+
+$('.imgAbout1').hideElement('.imgAbout1','.contentAbout1','.imgAbout2');
+$('.imgAbout3').hideElement('.imgAbout3','.contentAbout2','.imgAbout4');
+$('.imgAbout5').hideElement('.imgAbout5','.contentAbout3','.imgAbout6');
+$('.imgAbout5').hideElement('.imgAbout7','.contentAbout4','.imgAbout8');
+
+(function($) {
+    $.fn.displayElement = function(event,element,element2) {
+        $(event).click(function() {
+            $(element).show("slide", {direction: "left"}, 1000, function() {
+                $(event).hide("slide", {direction: "left"}, 1000, function() {
+                    $(element2).show(1000)
+                });
+            });
+        });
+    };
+})(jQuery);
+
+$('.imgAbout2').displayElement('.imgAbout2','.contentAbout1','.imgAbout1');
+$('.imgAbout4').displayElement('.imgAbout4','.contentAbout2','.imgAbout3');
+$('.imgAbout6').displayElement('.imgAbout6','.contentAbout3','.imgAbout5');
+$('.imgAbout6').displayElement('.imgAbout8','.contentAbout4','.imgAbout7');
+
 // script pout la boite modal connect
 $("#btnModalConnect").click(function () {
     $("#modalConnect").show(500, function () {
@@ -10,44 +44,3 @@ $("#closeConnect").click(function () {
         $("#modalConnect").css("display", "none")
     });
 });
-
-
-
-
-
-
-// $(document).ready(function() {
-//     $('#header, #section1, #section2').hide(4000);
-// });
-
-// $(document).ready(function () {
-//     $("#header, #section1, #section2, #section3, #section4, #section5, #footer").hide(10, function () {
-//         $("body").show(10, function () {
-//             $("#header").show(500, function () {
-//                 $("#section1").show(500, function () {
-//                     $("#section2").show(500, function () {
-//                         // $(document).scroll(function () {
-//                             $("#section3, #section4, #footer").show(1000);
-//                         });
-//                     // });
-//                 });
-//             });
-//         });
-//     });
-// });
-
-// $(document).ready(function () {
-//     $("#header, #section1, #section2, #section3, #section4, #section5, #footer").hide(10, function() {
-//         $("body").show(10, function () {
-//             $("#header").show("slide", {direction: "left"}, 1500, function() {
-//                 $("#section1").show("slide", {direction: "right"}, 1500, function() {
-//                     $("#section2").show("slide", {direction: "left"}, 1500, function() {
-//                         $(document).scroll(function() {
-//                             $("#section3", "#section4", "#footer").show(1000);
-//                         });
-//                     });
-//                 });
-//             });
-//         });
-//     });
-// });
