209d79770e8b30f261101b80a054112a7d6db079
Perriguey <yo44prg@icloud.com>
2020-05-22 15:30:18 +0200

    optimized css and js

--- app/views/front/layouts/head.php
+++ app/views/front/layouts/head.php
@@ -16,7 +16,7 @@
     <!-- Charge la bibliothèque jQuery ui -->
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk=" crossorigin="anonymous"></script>
     <!-- lien vers les feuilles de style -->
-    <link rel="stylesheet" href="app/public/style/header.css">
+    <!-- <link rel="stylesheet" href="app/public/style/header.css">
     <link rel="stylesheet" href="app/public/style/home.css">
     <link rel="stylesheet" href="app/public/style/footer.css">
     <link rel="stylesheet" href="app/public/style/contact.css">
@@ -26,11 +26,9 @@
     <link rel="stylesheet" href="app/public/style/about.css">
     <link rel="stylesheet" href="app/public/style/article.css">
     <link rel="stylesheet" href="app/public/style/game.css">
-    <link rel="stylesheet" href="app/public/style/space.css">
     <link rel="stylesheet" href="app/public/style/account.css">
     <link rel="stylesheet" href="app/public/style/usersArticle.css">
     <link rel="stylesheet" href="app/public/style/userSettings.css">
-    <link rel="stylesheet" href="app/public/style/stats.css">
     <link rel="stylesheet" href="app/public/style/usersWrite.css">
     <link rel="stylesheet" href="app/public/style/usersModify.css">
     <link rel="stylesheet" href="app/public/style/error404.css">
@@ -38,8 +36,8 @@
     <link rel="stylesheet" href="app/public/style/sitemap.css">
     <link rel="stylesheet" href="app/public/style/battleship.css">
     <link rel="stylesheet" href="app/public/style/power4.css">
-    <link rel="stylesheet" href="app/public/style/labyrinth.css">
-    <link rel="stylesheet" href="app/public/style/mediaQueries.css">
+    <link rel="stylesheet" href="app/public/style/mediaQueries.css"> -->
+    <link rel="stylesheet" href="app/public/style/opti.css">
     <!-- icône -->
     <link rel="icon" href="app/public/images/logo/icone.png">
     <title>JeuxVascript | <?= $title ?> </title>
